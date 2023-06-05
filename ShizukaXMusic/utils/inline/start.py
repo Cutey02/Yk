2e9a0959e5eed17be600cec6dc756655bf683e1b
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-06-05 11:12:46 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -120,6 +120,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 ]
             )
     buttons.append(
-        [InlineKeyboardButton(text=_["✪ ʙᴀᴅ ʙᴏʏ ✪"], url=f"http://t.me/badboybiografia")]
+        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")]
     )
     return buttons
